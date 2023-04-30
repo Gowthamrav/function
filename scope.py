655f1bd830c1b49bef22970a279ac75a46c633cc
gowtham <gowthamravi7270@gmail.com>
2023-04-30 09:57:29 +0530

    leart few more

--- scope.py
+++ scope.py
@@ -1,14 +1,18 @@
 class father:
     def fun(self):
         print('this is first print ')
+
+class father2:
+    def super(self):
+        print('this is first print ')        
 class mother(father):
     def fun(self):
         print('this is second print ')
         def fun1(self):
             super().fun(self)
             print('this is three print')
-maj=mother()
-maj.fun()           
+maj=father2()
+maj.super()           
 
                 
 
